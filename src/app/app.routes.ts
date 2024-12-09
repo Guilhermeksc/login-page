27524ed33156c6b11b350c4d62f541128f1dc67c
Guilherme <guilhermekscampos@gmail.com>
2024-12-09 11:17:32 -0300

    feat: att

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,13 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/signup/signup.component';
 import { UserComponent } from './pages/user/user.component';
 import { AuthGuard } from './services/auth-guard.service';
 
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+
 export const routes: Routes = [
-    {
-        path: "",
-        component: LoginComponent
-    },
-    {
-        path: "signup",
-        component: SignUpComponent
-    },
-    {
-        path: "user",
-        component: UserComponent,
-        canActivate: [AuthGuard]
-    }
+  { path: '', component: HomeComponent }, // Home na raiz
+  { path: 'login', component: LoginComponent }, // Login na rota /login
+  { path: '**', redirectTo: '' } // Redireciona rotas não encontradas para a Home
 ];
